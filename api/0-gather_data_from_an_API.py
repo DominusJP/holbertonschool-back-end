7b02345ed5cfd23ac3356a63c5dfb246883b505d
Juan Ferraro <135638564+DominusJP@users.noreply.github.com>
2024-07-05 00:48:42 -0300

    Update 0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,34 +1,35 @@
 #!/usr/bin/python3
-"""Script to use a REST API for a given employee ID, returns
-information about his/her TODO list progress"""
+"""0-gather_data_from_an_API task"""
+
+
 import requests
 import sys
 
 
+def get_employee(argv):
+    """get empleoye data"""
+    employee_id = argv[1]
+    user_link = "https://jsonplaceholder.typicode.com/users/"
+    user_response = requests.get("{}{}".format(user_link, employee_id))
+    user = user_response.json()
+    todo_link = "https://jsonplaceholder.typicode.com/todos?userId="
+    todo_response = requests.get("{}{}".format(todo_link, employee_id))
+    todos = todo_response.json()
+
+    task_completed = 0
+    for todo in todos:
+        if todo.get('completed'):
+            task_completed += 1
+
+    total_tasks = len(todos)
+    print(f"Employee {user.get('name')} is done with tasks"
+          f"({task_completed}/{total_tasks}):")
+
+    for todo in todos:
+        if todo.get('completed'):
+            print(f"\t {todo.get('title')}")
+
+
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"UsageError: python3 {__file__} employee_id(int)")
-        sys.exit(1)
-
-    API_URL = "https://jsonplaceholder.typicode.com"
-    EMPLOYEE_ID = sys.argv[1]
-
-    response = requests.get(
-        f"{API_URL}/users/{EMPLOYEE_ID}/todos",
-        params={"_expand": "user"}
-    )
-    data = response.json()
-
-    if not len(data):
-        print("RequestError:", 404)
-        sys.exit(1)
-
-    employee_name = data[0]["user"]["name"]
-    total_tasks = len(data)
-    done_tasks = [task for task in data if task["completed"]]
-    total_done_tasks = len(done_tasks)
-
-    print(f"Employee {employee_name} is done with tasks"
-          f"({total_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
-        print(f"\t {task['title']}")
+    if len(sys.argv) == 2:
+        get_employee(sys.argv)
